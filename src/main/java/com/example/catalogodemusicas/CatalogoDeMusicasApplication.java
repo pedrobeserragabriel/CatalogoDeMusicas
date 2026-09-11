74c139d62c4a35e4a17f6b4bbb0a6ced583228b6
pedrobeserragabriel <pedrobeserra.gabriel@uel.br>
2026-09-10 21:08:42 -0300

    Primeiro Commit

---
+++ src/main/java/com/example/catalogodemusicas/CatalogoDeMusicasApplication.java
@@ -0,0 +1,13 @@
+package com.example.catalogodemusicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoDeMusicasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogoDeMusicasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/catalogodemusicas/CatalogoDeMusicasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.catalogodemusicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoDeMusicasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
