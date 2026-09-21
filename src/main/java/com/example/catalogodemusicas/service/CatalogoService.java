218f53fecfad7ff421b658eb870ba1b74ddce3ec
Pedro <pedrobeserra.gabriel@uel.br>
2026-09-20 23:01:38 -0300

    Service

--- src/main/java/com/example/catalogodemusicas/service/CatalogoService.java
+++ src/main/java/com/example/catalogodemusicas/service/CatalogoService.java
@@ -1,7 +1,13 @@
 package com.example.catalogodemusicas.service;
 
+import com.example.catalogodemusicas.repository.CatalogoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CatalogoService {
+    @Autowired
+    private CatalogoRepository catalogoRepository;
+
+
 }
