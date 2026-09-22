67c80435f42e58126ecdc9e837e7c4b3530fa3f7
Pedro <pedrobeserra.gabriel@uel.br>
2026-09-22 01:08:40 -0300

    Criação do código inicial do sql

--- sql/SQLCatalogo.sql
+++ sql/SQLCatalogo.sql
@@ -1,2 +1,12 @@
-CREATE DATABASE SQLCatalogo;
-USE SQLCatalogo;
+CREATE DATABASE catalogo_musicas;
+
+USE catalogo_musicas;
+
+CREATE TABLE musicas (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    album VARCHAR(100) NOT NULL,
+    artistas VARCHAR(100) NOT NULL,
+    tempo_segundos INT NOT NULL,
+    arquivo_audio VARCHAR(255)
+);
